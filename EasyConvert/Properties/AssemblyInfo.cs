74c30eeb75fb3856b1f78215c826f328a98bdb70
inightsword <i.nightsword@proton.me>
2024-11-13 04:49:38 +0300

    Upload Project

---
+++ EasyConvert/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EasyConvert")]
+[assembly: AssemblyDescription("Simple and easy tool to convert,encrypt and hashing.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Github.com/inightsword")]
+[assembly: AssemblyProduct("EasyConvert")]
+[assembly: AssemblyCopyright("Copyright © Night Sword")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2a6334f1-d0ae-4d44-bb08-805641ae4270")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
